2999823e572d7dfa1061a8873ad06bef8ae91c52
yaoguangxing <yaoguangxing@geostar.com.cn>
2022-03-06 01:08:10 +0800

    01_Java基础面试必问要点之概念以及原理的介绍

---
+++ CodeFolder/01_JavaBase/src/main/java/com/huaxing/javabase/HelloWord.java
@@ -0,0 +1,12 @@
+package com.huaxing.javabase;
+
+/**
+ * @Auther: yaogx
+ * @Date：2022/3/5 21:49
+ * @Description: TODO
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HuaXing HelloWord !");
+    }
+}
